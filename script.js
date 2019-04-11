11bb8a085f638a16983f7538b5cd4bb396f01e3c
MateuszGra <mxgrabowski@gmail.com>
2019-04-11 14:12:37 +0200

    test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     const leds = [
         [
             document.querySelector(".oneTensHours"),
@@ -33,22 +33,23 @@
         ],
         [
             document.querySelector(".tensMinutes"),
-            document.querySelector(".unitesMinutes"), 
+            document.querySelector(".unitesMinutes"),
         ],
         [
             document.querySelector(".tensSeconds"),
             document.querySelector(".unitesSeconds"),
         ],
-        [                
-            document.querySelector(".numberOption"),  
+        [
+            document.querySelector(".numberOption"),
+            document.querySelector(".airQuality"),
         ],
     ];
     const optionsLeds = [
         document.querySelector(".numberOptionLed"),
+        document.querySelector(".airQualityLed"),
     ];
     const menu = document.getElementById('menu');
     const options = document.querySelector(".options");
-    const body = document.querySelector("body");
 
     const colors = [
         'red',
@@ -97,6 +98,7 @@
         document.getElementById('ledsOff23'),
         document.getElementById('ledsOff24'),
     ]
+
     let draw;
     draw = Math.random() * 7;
     draw = Math.round(draw);
@@ -107,105 +109,92 @@
 
     menuLeds[draw].classList.add(classOn);
 
-    for (let i=0; i<colors.length; i++) {
-        menuLeds[i].addEventListener("click", function(e){
-            e.stopPropagation(); 
+    for (let i = 0; i < colors.length; i++) {
+        menuLeds[i].addEventListener("click", function (e) {
             classOn = colors[i];
             classOff = colors[i] + 'Off';
             classNumber = colors[i] + 'Number'
-            for (let o=0; o<menuLeds.length; o++) {
+            for (let o = 0; o < menuLeds.length; o++) {
                 menuLeds[o].classList.remove(colors[o]);
                 optionsLeds[0].classList.remove(colors[o]);
             }
             menuLeds[i].classList.add(colors[i]);
 
-            if(leds[3][0].classList[1] == 'none'){
+            if (leds[3][0].classList[1] == 'none') {
                 optionsLeds[0].classList.add(classOff);
-            }else{
+            } else {
                 optionsLeds[0].classList.add(classOn);
             }
         });
-        menuLeds[i].addEventListener("touch", function(e){
-            e.stopPropagation(); 
+        menuLeds[i].addEventListener("touch", function (e) {
             classOn = colors[i];
             classOff = colors[i] + 'Off';
             classNumber = colors[i] + 'Number'
-            for (let o=0; o<menuLeds.length; o++) {
+            for (let o = 0; o < menuLeds.length; o++) {
                 menuLeds[o].classList.remove(colors[o]);
                 optionsLeds[0].classList.remove(colors[o]);
             }
             menuLeds[i].classList.add(colors[i]);
 
-            if(leds[3][0].classList[1] == 'none'){
+            if (leds[3][0].classList[1] == 'none') {
                 optionsLeds[0].classList.add(classOff);
-            }else{
+            } else {
                 optionsLeds[0].classList.add(classOn);
             }
         });
     }
 
     optionsLeds[0].classList.add(classOff);
-    leds[6][0].addEventListener('click', function(e){
-        e.stopPropagation(); 
-        for (let i=0; i<optionsLeds[i].length; i++) {
-            for (let o=0; o<colors.length; o++) {
+    leds[6][0].addEventListener('click', function (e) {
+        for (let i = 0; i < optionsLeds[i].length; i++) {
+            for (let o = 0; o < colors.length; o++) {
                 optionsLeds[i].classList.remove(colors[o]);
             }
         }
         optionsLeds[0].classList.toggle(classOn);
 
-        for (let i=0; i<leds[3].length; i++) {
+        for (let i = 0; i < leds[3].length; i++) {
             leds[3][i].classList.toggle('none');
             leds[4][i].classList.toggle('none');
             leds[5][i].classList.toggle('none');
         }
     });
-    leds[6][0].addEventListener('touch', function(e){
-        e.stopPropagation(); 
-        for (let i=0; i<optionsLeds[i].length; i++) {
-            for (let o=0; o<colors.length; o++) {
+    leds[6][0].addEventListener('touch', function (e) {
+        for (let i = 0; i < optionsLeds[i].length; i++) {
+            for (let o = 0; o < colors.length; o++) {
                 optionsLeds[i].classList.remove(colors[o]);
             }
         }
         optionsLeds[0].classList.toggle(classOn);
 
-        for (let i=0; i<leds[3].length; i++) {
+        for (let i = 0; i < leds[3].length; i++) {
             leds[3][i].classList.toggle('none');
             leds[4][i].classList.toggle('none');
             leds[5][i].classList.toggle('none');
         }
     });
 
-    menu.addEventListener('click', function(e){
-        e.stopPropagation(); 
+    menu.addEventListener('click', function (e) {
         options.classList.toggle('none');
     });
-    menu.addEventListener('touch', function(e){
-        e.stopPropagation(); 
+    menu.addEventListener('touch', function (e) {
         options.classList.toggle('none');
     });
 
-    body.addEventListener('click', function(e){
-        options.classList.add('none');
-    });
-    body.addEventListener('touch', function(e){
-        options.classList.add('none');
-    });
-
-    function showTime(variable, section, sectionNumber){
+    function showTime(variable, section, sectionNumber) {
         menu.classList.add(classOn);
-        for (let i=0; i<ledsOff.length; i++) {
+        for (let i = 0; i < ledsOff.length; i++) {
             ledsOff[i].classList.add(classOff);
         }
-        for (let i=0; i<leds[3].length; i++) {
+        for (let i = 0; i < leds[3].length; i++) {
             leds[3][i].classList.add(classNumber);
             leds[4][i].classList.add(classNumber);
             leds[5][i].classList.add(classNumber);
         }
-        for (let i=0; i<leds[6].length; i++) {
+        for (let i = 0; i < leds[6].length; i++) {
             leds[6][i].classList.add(classNumber);
         }
-        for (let i=0; i<optionsLeds.length; i++) {
+        for (let i = 0; i < optionsLeds.length; i++) {
             optionsLeds[i].classList.add(classOff);
         }
 
@@ -216,71 +205,71 @@
         sectionNumber[0].textContent = tens;
         sectionNumber[1].textContent = unities;
 
-        if(unities >= 8){
+        if (unities >= 8) {
             section[6].classList.add(classOn);
             unities = unities - 8
         }
-        if(unities >= 4){
+        if (unities >= 4) {
             section[5].classList.add(classOn);
             unities = unities - 4
         }
-        if(unities >= 2){
+        if (unities >= 2) {
             section[4].classList.add(classOn);
             unities = unities - 2
         }
-        if(unities >= 1){
+        if (unities >= 1) {
             section[3].classList.add(classOn);
         }
 
-        if(tens >= 4){
+        if (tens >= 4) {
             section[2].classList.add(classOn);
             tens = tens - 4
         }
-        if(tens >= 2){
+        if (tens >= 2) {
             section[1].classList.add(classOn);
             tens = tens - 2
         }
-        if(tens >= 1){
+        if (tens >= 1) {
             section[0].classList.add(classOn);
         }
     }
 
-    function reset(){
-        for (let i=0; i<leds.length; i++) {
-            for (let o=0; o<leds[i].length; o++) {
-                for (let z=0; z<colors.length; z++) {
+    function reset() {
+        for (let i = 0; i < leds.length; i++) {
+            for (let o = 0; o < leds[i].length; o++) {
+                for (let z = 0; z < colors.length; z++) {
                     leds[i][o].classList.remove(colors[z]);
                 }
             }
         }
-        for (let i=0; i<ledsOff.length; i++) {
-            for (let o=0; o<colors.length; o++) {
+        for (let i = 0; i < ledsOff.length; i++) {
+            for (let o = 0; o < colors.length; o++) {
                 ledsOff[i].classList.remove(colors[o] + 'Off');
             }
         }
-        for (let i=0; i<leds[3].length; i++) {
-            for (let o=0; o<colors.length; o++) {
+        for (let i = 0; i < leds[3].length; i++) {
+            for (let o = 0; o < colors.length; o++) {
                 leds[3][i].classList.remove(colors[o] + 'Number');
                 leds[4][i].classList.remove(colors[o] + 'Number');
                 leds[5][i].classList.remove(colors[o] + 'Number');
             }
         }
-        for (let i=0; i<leds[6].length; i++) {
-            for (let o=0; o<colors.length; o++) {
+        for (let i = 0; i < leds[6].length; i++) {
+            for (let o = 0; o < colors.length; o++) {
                 leds[6][i].classList.remove(colors[o] + 'Number');
             }
         }
-        for (let i=0; i<optionsLeds.length; i++) {
-            for (let o=0; o<colors.length; o++) {
+        for (let i = 0; i < optionsLeds.length; i++) {
+            for (let o = 0; o < colors.length; o++) {
                 optionsLeds[i].classList.remove(colors[o] + 'Off');
             }
         }
-        for (let i=0; i<colors.length; i++) {
+        for (let i = 0; i < colors.length; i++) {
             menu.classList.remove(colors[i]);
         }
     }
 
-    setInterval(function(){ 
+    setInterval(function () {
         let currentDate = new Date();
         let hours = currentDate.getHours();
         let minutes = currentDate.getMinutes();
@@ -289,6 +278,19 @@
         reset();
         showTime(hours, leds[0], leds[3]);
         showTime(minutes, leds[1], leds[4]);
-        showTime(seconds, leds[2],leds[5]);
+        showTime(seconds, leds[2], leds[5]);
     }, 100);
+
+
+
+    let http = new XMLHttpRequest();
+    const url = "http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/129";
+    http.open("GET", url);
+
+
+    console.log(http.responseText);
+
+
+
+
 })();
