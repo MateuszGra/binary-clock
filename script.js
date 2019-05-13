c0bcf2bb297510c7580870e47aee85670b2ecd79
MateuszGra <mxgrabowski@gmail.com>
2019-05-13 23:11:36 +0200

    comments

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 (() => {
+    //clock wheel
     const leds = [
         [
             document.getElementById("oneTensHours"),
@@ -30,6 +31,7 @@
             document.getElementById("eightUnitiesSeconds"),
             document.getElementById("ledsOff20"),
         ],
+        //digital clock numbers
         [
             document.getElementById("tensHours"),
             document.getElementById("unitesHours"),
@@ -45,103 +47,98 @@
     ];
 
     const subtitlesLeds = [
-        document.querySelector(".numberOption"),
-        document.querySelector(".weatherOption"),
-        document.querySelector(".temp"),
-        document.querySelector(".weatherIcon"),
-        document.querySelector(".city"),
-        document.querySelector(".name"),
-        document.querySelector(".fa-info-circle"),
-        document.querySelector(".fullInfo"),
-        document.querySelector(".tempFullInfo"),
-        document.querySelector(".weatherFullInfo"),
-        document.querySelector(".cityFullInfo"),
-        document.querySelector(".pressureFullInfo"),
-        document.querySelector(".humidityFullInfo"),
-        document.querySelector(".windFullInfo"),
-        document.querySelector(".sunriseFullInfo"),
-        document.querySelector(".SunsetFullInfo"),
-        document.querySelector(".cordFullInfo"),
-        document.querySelector(".updateFullInfo"),
+        document.querySelector(".numberOption"), //menu - option digital clock
+        document.querySelector(".weatherOption"), //menu - option weather
+        document.querySelector(".temp"), //weather - temperature
+        document.querySelector(".weatherIcon"), //weather - icon
+        document.querySelector(".city"), //menu - input city
+        document.querySelector(".name"), //weather - city info
+        document.querySelector(".fa-info-circle"), //weather - info icon
+        document.querySelector(".fullInfo"), //weather - full raport - wrapper
+        document.querySelector(".tempFullInfo"), //weather - full raport - temperature
+        document.querySelector(".weatherFullInfo"), //weather - full raport- weather
+        document.querySelector(".cityFullInfo"), //weather - full raport- city
+        document.querySelector(".pressureFullInfo"), //weather - full raport- pressure
+        document.querySelector(".humidityFullInfo"), //weather - full raport- humiditi
+        document.querySelector(".windFullInfo"), //weather - full raport- wind
+        document.querySelector(".sunriseFullInfo"), //weather - full raport- humiditi
+        document.querySelector(".SunsetFullInfo"), //weather - full raport- sunset
+        document.querySelector(".cordFullInfo"), //weather - full raport- coordinate
+        document.querySelector(".updateFullInfo"), //weather - full raport- update time
     ];
 
     const optionsLeds = [
-        document.querySelector(".numberOptionLed"),
-        document.querySelector(".weatherOptionLed"),
+        document.querySelector(".numberOptionLed"), //menu - option digital clock - info led
+        document.querySelector(".weatherOptionLed"), //menu - option weather - info led
     ];
+
+    const menu = document.getElementById('menu'); //menu - button
+    const options = document.querySelector(".options"); //menu - wrapper
+    const kiwi = document.querySelector(".fa-kiwi-bird"); //menu- color setting - kiwi icon
+    const menuLeds = document.querySelectorAll("#wheel"); //menu- color setting
+    const weatherSection = document.querySelector(".weather"); //weather all section
 
     const weatherIcons = [
         'fa-cloud', 'fa-sun', 'fa-moon', 'fa-cloud-moon', 'fa-cloud-showers-heavy', 'fa-cloud-sun-rain', 'fa-cloud-moon-rain', 'fa-bolt', 'fa-snowflake', 'fa-smog', 'fa-exclamation-triangle',
     ]
-
-    const menu = document.getElementById('menu');
-    const options = document.querySelector(".options");
-    const weatherSection = document.querySelector(".weather");
-    const kiwi = document.querySelector(".fa-kiwi-bird");
-    const weatherInfo = document.querySelector(".weather");
-
     const colors = [
         'red', 'yellow', 'orange', 'mint', 'ice', 'blue', 'purple', 'pink',
     ];
 
-    const menuLeds = [
-        document.getElementById("wheel1"),
-        document.getElementById("wheel2"),
-        document.getElementById("wheel3"),
-        document.getElementById("wheel4"),
-        document.getElementById("wheel5"),
-        document.getElementById("wheel6"),
-        document.getElementById("wheel7"),
-        document.getElementById("wheel8"),
-    ];
+    let weatherApiUpdate; //weather API (set interval)
+    let discoBirdInterval; //color change interval
+    let tempoColor = 'off'; // color change status (on/off)
+    let draw; //current color 
 
-    let weatherApiUpdate;
-    let discoBirdInterval;
-    let tempoColor = 'off';
-    let draw;
+    //current date and time
     let currentDate = new Date();
     let hours = currentDate.getHours();
     let minutes = currentDate.getMinutes();
     let seconds = currentDate.getSeconds();
 
-    if (localStorage.getItem('color') != null) {
-        draw = localStorage.getItem('color')
-        draw = parseInt(draw);
-    } else {
-        draw = Math.random() * 7;
-        draw = Math.round(draw);
-        localStorage.setItem('color', draw);
-    }
-    if (localStorage.getItem('city') != null) {
-        subtitlesLeds[4].value = localStorage.getItem('city');
-    } else {
-        subtitlesLeds[4].value = 'Wrocław';
-    }
-
-    if (localStorage.getItem('digitalClock') == 'on') {
-        for (let i = 0; i < leds[3].length; i++) {
-            leds[3][i].classList.remove('none');
-            leds[4][i].classList.remove('none');
-            leds[5][i].classList.remove('none');
-            optionsLeds[0].classList.add(colors[draw]);
+    //load and set local storage options
+    loadOptions = () => {
+        if (localStorage.getItem('color') != null) {
+            draw = localStorage.getItem('color')
+            draw = parseInt(draw);
+        } else {
+            draw = Math.random() * 7;
+            draw = Math.round(draw);
+            localStorage.setItem('color', draw);
         }
-    } else {
-        for (let i = 0; i < leds[3].length; i++) {
-            leds[3][i].classList.add('none');
-            leds[4][i].classList.add('none');
-            leds[5][i].classList.add('none');
-            optionsLeds[0].classList.remove(colors[draw] + 'Off');
+        if (localStorage.getItem('city') != null) {
+            subtitlesLeds[4].value = localStorage.getItem('city');
+        } else {
+            subtitlesLeds[4].value = 'Wrocław';
+        }
+
+        if (localStorage.getItem('digitalClock') == 'on') {
+            for (let i = 0; i < leds[3].length; i++) {
+                leds[3][i].classList.remove('none');
+                leds[4][i].classList.remove('none');
+                leds[5][i].classList.remove('none');
+                optionsLeds[0].classList.add(colors[draw]);
+            }
+        } else {
+            for (let i = 0; i < leds[3].length; i++) {
+                leds[3][i].classList.add('none');
+                leds[4][i].classList.add('none');
+                leds[5][i].classList.add('none');
+                optionsLeds[0].classList.remove(colors[draw] + 'Off');
+            }
+        }
+
+        if (localStorage.getItem('weather') == 'off') {
+            weatherSection.classList.add('none');
+            optionsLeds[1].classList.add(colors[draw] + 'Off');
+        } else {
+            weatherSection.classList.remove('none');
+            optionsLeds[1].classList.add(colors[draw]);
         }
     }
+    loadOptions();
 
-    if (localStorage.getItem('weather') == 'off') {
-        weatherSection.classList.add('none');
-        optionsLeds[1].classList.add(colors[draw] + 'Off');
-    } else {
-        weatherSection.classList.remove('none');
-        optionsLeds[1].classList.add(colors[draw]);
-    }
-
+    //add colors to all (options,clock, weather...)
     addColors = () => {
         menu.classList.add(colors[draw]);
         menuLeds[draw].classList.add(colors[draw]);
@@ -165,6 +162,7 @@
         kiwi.classList.add(colors[draw] + 'OffNumber');
     }
 
+    //show time at binary and digital clock
     showTime = (variable, section, sectionNumber) => {
         let tens = Math.floor(variable / 10);
         let unities = variable - tens * 10;
@@ -201,6 +199,7 @@
         }
     }
 
+    //reset all colors before set new one
     reset = () => {
         for (let i = 0; i < leds.length; i++) {
             for (let o = 0; o < leds[i].length; o++) {
@@ -239,6 +238,7 @@
         }
     }
 
+    //time set interwal 
     setInterval(() => {
         currentDate = new Date();
         hours = currentDate.getHours();
@@ -252,12 +252,14 @@
         showTime(seconds, leds[2], leds[5]);
     }, 100);
 
+    //get info from weather API
     weatherApi = () => {
+        //auxiliary function to round results
         round = (n, k) => {
             const factor = Math.pow(10, k);
             return Math.round(n * factor) / factor;
         }
-
+        //auxiliary function to change country code to full country name
         decodeCountry = n => {
             const countryCode = ['AF', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'HR', 'CU', 'CW', 'CY', 'CZ', 'CI', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'SZ', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY', 'HT', 'HM', 'VA', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM', 'JP', 'JE', 'JO', 'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU', 'MO', 'MK', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR', 'QA', 'RO', 'RU', 'RW', 'RE', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SX', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS', 'SS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK', 'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ', 'VU', 'VE', 'VN', 'VG', 'VI', 'WF', 'EH', 'YE', 'ZM', 'ZW', 'AX'];
             const country = ['Afghanistan', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia, Plurinational State of', 'Bonaire, Sint Eustatius and Saba', 'Bosnia and Herzegovina', 'Botswana', 'Bouvet Island', 'Brazil', 'British Indian Ocean Territory', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo', 'Congo, the Democratic Republic of the', 'Cook Islands', 'Costa Rica', 'Croatia', 'Cuba', 'Curaçao', 'Cyprus', 'Czechia', 'Côte d’Ivoire', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Eswatini', 'Ethiopia', 'Falkland Islands (Malvinas)', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard Island and McDonald Islands', 'Holy See (Vatican City State)', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran, Islamic Republic of', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Korea, Democratic People’s Republic of', 'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', 'Lao People’s Democratic Republic', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libyan Arab Jamahiriya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macao', 'Macedonia, the former Yugoslav Republic of', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestinian Territory, Occupied', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Romania', 'Russian Federation', 'Rwanda', 'Réunion', 'Saint Barthélemy', 'Saint Helena, Ascension and Tristan da Cunha', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin (French part)', 'Saint Pierre and Miquelon', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia and the South Sandwich Islands', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Svalbard and Jan Mayen', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China', 'Tajikistan', 'Tanzania, United Republic of', 'Thailand', 'Timor-Leste', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Virgin Islands, British', 'Virgin Islands, U.S.', 'Wallis and Futuna', 'Western Sahara', 'Yemen', 'Zambia', 'Zimbabwe', 'Åland Islands'];
@@ -269,7 +271,7 @@
             }
             return n;
         }
-
+        //auxiliary function to change wind deg to direction
         windDirection = n => {
             if (n > 11.25 && n <= 33.75) {
                 return 'NNE';
@@ -305,14 +307,14 @@
                 return 'N';
             }
         }
-
+        //auxiliary function to delete if some API info is not defined
         delUndefined = (n, y) => {
             if (n == undefined) {
                 y = '';
             }
             return y;
         }
-
+        //auxiliary function to add zero to result (3am to 03am)
         addZero = n => {
             if (n < 10) {
                 n = '0' + n;
@@ -320,6 +322,7 @@
             return n;
         }
 
+        //get info from API and set information in HTML
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + encodeURI(subtitlesLeds[4].value) + "&APPID=18a4fba4ee73407fc5b7e49ba72b3fc4")
             .then(resp => resp.json())
             .then(resp => {
@@ -384,6 +387,7 @@
         weatherApiUpdate = setInterval(weatherApi, 60000);
     }
 
+    //automatic change of colors
     discoBird = () => {
         draw++;
         if (draw >= colors.length) {
@@ -410,7 +414,7 @@
         }
     }
 
-    //events
+    //////////////////////////////////////// event listeners ////////////////////////////////////////
 
     colorClick = n => {
         for (let o = 0; o < menuLeds.length; o++) {
@@ -535,11 +539,11 @@
         numberOptionClick();
     });
 
-    weatherInfo.addEventListener('click', (e) => {
+    weatherSection.addEventListener('click', (e) => {
         e.stopPropagation();
         subtitlesLeds[7].classList.toggle('none');
     });
-    weatherInfo.addEventListener('touch', (e) => {
+    weatherSection.addEventListener('touch', (e) => {
         e.stopPropagation();
         subtitlesLeds[7].classList.toggle('none');
     });
